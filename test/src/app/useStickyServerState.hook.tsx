5074b8cf4ce4589be81f6c3e300e3b15d6c07659
ChurroC <66274903+ChurroC@users.noreply.github.com>
2024-08-21 16:58:03 +0000

    trying to use hashes to find changes

--- test/src/app/useStickyServerState.hook.tsx
+++ test/src/app/useStickyServerState.hook.tsx
@@ -19,27 +19,60 @@ function useOnChange(
     }, dependancies);
 }
 
+function hashCode(word: string) {
+    let hash = 0
+    let chr
+    if (word.length === 0) return hash;
+    for (let i = 0; i < word.length; i++) {
+        chr = word.charCodeAt(i)
+        hash = (hash << 5) - hash + chr
+        hash |= 0
+    }
+    return hash
+  }
+
 export function useStickyServerState<CookieType>(
-    name: string,
+    key: string,
     defaultValue: CookieType
 ) {
-    const serverCookie = useCookie(name);
+    const serverCookie = useCookie(key);
     const [cookie, setCookie] = useState<CookieType>(serverCookie ? JSON.parse(serverCookie) as CookieType : defaultValue);
 
-    // This is checking for external changed in cookies
-    useEffect(() => {});
-
     // Update cookies when cookie state changes
+    // Currently have a race condition for localStorage
     useOnChange(() => {
         if (typeof cookieStore !== "undefined") {
-            void cookieStore.set(name, JSON.stringify(cookie));
+            cookieStore.set(key, JSON.stringify(cookie));
         } else {
-            document.cookie = `${name}=${JSON.stringify(cookie)};`;
+            document.cookie = `${key}=${JSON.stringify(cookie)};`;
         }
+        // This is easier than broadcast channel to modify the theme
+        localStorage.setItem(key, hashCode(JSON.stringify(cookie)).toString());
     }, [cookie]);
+
+    // This is checking for external changed in cookies
+    useEffect(() => {
+        // This is when another tabs theme changes
+        // Was going to use broadcast api but this is easier since it runs on other tabs and doesnt run if localstorage is set to the value
+        function onStorageChange({ key: newKey, newValue }: StorageEvent) {
+            console.log(key, newKey, newValue);
+            // New value is just a hash to see if it is the same
+            // If not then it will update the cookie
+            if (key === newKey && newValue) {
+                if (newValue !== hashCode(JSON.stringify(cookie)).toString()) {
+                    if (typeof cookieStore !== "undefined") {
+                        setCookie(JSON.parse(cookieStore.get(key)) as CookieType);
+                    } else {
+                        setCookie(JSON.parse(document.cookie.match('(^|;)\\s*' + key + '\\s*=\\s*([^;]+)')?.pop() ?? '') as CookieType);
+                    }
+                }
+            }
+        }
+        window.addEventListener("storage", onStorageChange);
+        console.log("added event listener");
+
+        return () => window.removeEventListener("storage", onStorageChange);
+    }, []);
 
     return [cookie, setCookie] as const;
 }
-
-// maybe gives info on cookie expiry data
-function cookieInfo() {}
